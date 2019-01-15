f9998003f4ad7d326df089f047fe17a6cddc104f
DahunJeongINU <JungDH@inu.ac.kr>
2019-01-15 19:24:58 +0900

    Edit a Detecor.py
    Deteror.py의 눈동자를 찾는거를 해보려 하였으나 실패로 포기

--- detector.py
+++ detector.py
@@ -34,14 +34,27 @@ while True:
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0),2)
             radius = 0.05*(w)
+            eye_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+            eye_color = roi_color[ey:ey+eh, ex:ex+ew]
+
             #간단하게 눈으로 잡은 공간의 중간부분을 눈의 중점으로 두고 잡았음
             #그렇기에 당연하게도 눈이 몰리는 경우를 못잡음
             #cv2.circle(roi_color, (int(ex+ew/2),int(ey+eh/2)), int(radius), (0,0,255), 1)
+            
+            #dst = cv2.cornerHarris(gray, 3, 5, 0.04)
+            dst = cv2.cornerHarris(eye_gray, 2, 3, 0.08)
+            #cornerHarris(gray,   #img: 이미지 데이터
+            #          2,       #blockSize: Corner detection을 위해 고려되는 인접 픽셀 크기
+            #          3,       #ksize: Sobel derivative의 Aperture  파라미터
+            #          0.04,  #k, Harris detector 파라미터)
+            
+            #dst - cv2.dilate(dst, None)
+
+            #frame[dst>0.01*dst.max()] = [0,255,0]
+            #eye_color[dst>0.01*dst.max()] = [0,255,0]
              
+    cv2.imshow('Video', roi_color)
 
-        
-
-    cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         video.realease()
