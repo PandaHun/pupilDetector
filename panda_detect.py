efcb8c5c1b18994b8ddd48a86a8e2b7d538ba553
DahunJeongINU <JungDH@inu.ac.kr>
2019-01-18 13:34:01 +0900

    Update panda_detect.py
    Filter를 활용해서 edge Detection을 할려 하였으나 조금 더 고려하고 계산해봐야할 것 같음.
    현재는 epsilon을 활용해서 잡는 포인트가 잘 잡히는 것으로 추정됨

--- panda_detect.py
+++ panda_detect.py
@@ -23,29 +23,35 @@ while True:
     ret, frame = capture.read()
 
     #For the processing the frame. so Convert the Color to gray
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_vid = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
+    #filter the frame that canny edge
+    filter = cv2.Canny(gray, 45, 45)
+
     #Detect the face using Cascade
     eyes = eyeCascade.detectMultiScale(
         gray,
-        scaleFactor = 1.3,
+        scaleFactor = 1.1,
         minNeighbors = 10,
         minSize = (45,45),
         flags = cv2.CASCADE_SCALE_IMAGE
     )
     irises = []
+    #Process that Detect Eyes
     for (ex,ey,ew,eh) in eyes:
         iris_w = int((ex+epsilon) + float((ew / 2)+epsilon))
         iris_h = int((ey+epsilon) + float((eh / 2)+epsilon))
         irises.append([np.float32(iris_w), np.float32(iris_h)])
+    #     print(iris_w, iris_h)
+    #    cv2.rectangle(gray, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
+        cv2.circle(gray, (iris_w,iris_h),int(float((iris_h/32)+epsilon)), (0,255,0), 2)
 
-        print(iris_w, iris_h)
-
-        cv2.rectangle(gray, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
-
-    
+    #Show the frame
     #cv2.imshow('Original Video', frame)
     cv2.imshow('Gray Video', gray)
+    cv2.imshow('Edge Video', filter)
+
 
     if cv2.waitKey(1) & 0xFF ==ord('q'):
         capture.release()
